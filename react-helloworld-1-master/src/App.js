17f2c20e1bf4d791efd0e44414be6c20bbc31ac9
Sajjad Tareen <sajjadtareen@hotmail.com>
2020-11-09 03:40:08 +0500

    App files commit

---
+++ react-helloworld-1-master/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+    <h1>Hello Team</h1>
+    <p>Conclusion By Sajjad </p>
+  </div>
+  );
+}
+
+export default App;
